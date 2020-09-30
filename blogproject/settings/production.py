df5b0293700298d6b15608830772143896b526e0
rogers <3143798927@qq.com>
2020-09-30 10:32:44 +0800

    “second commit

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -4,5 +4,5 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['hellodjango-blog-tutorial-demo.zmrenwu.com']
+ALLOWED_HOSTS = ['123.57.177.219']
 HAYSTACK_CONNECTIONS['default']['URL'] = 'http://hellodjango_blog_tutorial_elasticsearch:9200/'
